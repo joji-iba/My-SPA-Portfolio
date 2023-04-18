ce2178cbaf0bbae12ec2b7148e57e741be7cc241
joji <jmry18.0921@gmail.com>
2023-04-18 16:18:49 +0900

    ヘッダーメニュー実装

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,68 @@
+import Link from 'next/link';
+import Logo from './Logo';
+import { useRouter } from 'next/router';
+import { TwitterIcon, GithubIcon } from './Icons';
+import { motion } from 'framer-motion';
+
+// ヘッダーリンクのコンポーネント
+const CustomLink = ({ href, title, className = '' }) => {
+  // 各ページ滞在時のメニューの下線表示キープ（パス情報と各リンクの一致を検知）
+  const router = useRouter();
+
+  return (
+    <Link href={href} className={`${className} relative group`}>
+      {title}
+      <span
+        className={`
+      h-[1px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300
+      ${router.asPath === href ? 'w-full' : 'w-0'}`}
+      >
+        &nbsp;
+      </span>
+    </Link>
+  );
+};
+
+export default function NavBar() {
+  return (
+    <header className="w-full px-32 py-8 font-medium flex items-center justify-between bg-white border-b border-gray-200 text-gray-700">
+      <div className="container mx-auto flex flex-row items-center p-5">
+        {/* ヘッダーメニュー */}
+        <nav>
+          <CustomLink href="/" title="Home" className="mr-4" />
+          <CustomLink href="/about" title="About" className="mx-4" />
+          <CustomLink href="/skills" title="Skills" className="mx-4" />
+          <CustomLink href="/projects" title="Projects" className="mx-4" />
+          <CustomLink href="/contact" title="Contact" className="ml-4" />
+        </nav>
+
+        {/* SNSアイコン */}
+        <nav className="flex items-center justify-center flex-wrap">
+          <motion.a
+            href="https://mobile.twitter.com/Joji0921jojo"
+            target={'_blank'}
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+            className="w-6 mr-3"
+          >
+            <TwitterIcon />
+          </motion.a>
+          <motion.a
+            href="https://github.com/joji-iba"
+            target={'_blank'}
+            className="w-6 ml-3"
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <GithubIcon />
+          </motion.a>
+        </nav>
+
+        {/* ロゴ */}
+        <div className="absolute left-[50%] top-2 translate-x-[-50%]">
+          <Logo />
+        </div>
+      </div>
+    </header>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,26 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import NavBar from 'components/NavBar';
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Montserrat } from 'next/font/google';
+import Head from 'next/head';
+
+// ページ全体のフォント読込
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  variable: '--font-mont',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+      </Head>
+      <main className={`${montserrat.variable} font-mont bg-light min-h-screen`}>
+        <NavBar />
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
