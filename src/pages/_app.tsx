a000a900e7bb137ad48ef6bf8c4da45f43915b65
joji <jmry18.0921@gmail.com>
2023-04-24 22:19:58 +0900

    FOUC回避

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import type { AppProps } from 'next/app';
 import { Montserrat } from 'next/font/google';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import { Footer } from 'components/Footer';
 import NavBar from 'components/NavBar';
 
@@ -15,21 +16,38 @@ const montserrat = Montserrat({
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
+
+  // FOUC回避の記述
+  const [show_screen, setShowScreen] = useState(false);
+
+  useEffect(() => {
+    setShowScreen(true);
+  }, []);
+
   return (
     <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-      </Head>
-      <main
-        className={`${montserrat.variable} font-mont bg-light dark:bg-dark min-h-screen`}
-      >
-        <NavBar />
-        <AnimatePresence mode="wait">
-          <Component key={router.asPath} {...pageProps} />
-        </AnimatePresence>
-        <Footer />
-      </main>
+      {show_screen ? (
+        <>
+          <Head>
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1.0"
+            />
+            <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+          </Head>
+          <main
+            className={`${montserrat.variable} font-mont bg-light dark:bg-dark min-h-screen`}
+          >
+            <NavBar />
+            <AnimatePresence mode="wait">
+              <Component key={router.asPath} {...pageProps} />
+            </AnimatePresence>
+            <Footer />
+          </main>
+        </>
+      ) : (
+        ''
+      )}
     </>
   );
 }
