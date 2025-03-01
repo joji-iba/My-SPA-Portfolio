00207edcecc97564736baa924a1a80749fe1339a
joji <jmry18.0921@gmail.com>
2025-03-01 23:08:31 +0900

    resolve hydration error

--- src/components/ThemeImage.tsx
+++ src/components/ThemeImage.tsx
@@ -7,12 +7,26 @@ import IconImageDark from '../../public/images/patternC02.png';
 
 export default function ThemeImage() {
   // ダークモードでの画像切替
+  const [mounted, setMounted] = useState(false);
   const [imgSrc, setImgSrc] = useState(IconImage);
 
   useEffect(() => {
+    setMounted(true);
     const theme = localStorage.getItem('theme') || 'light';
     setImgSrc(theme === 'dark' ? IconImageDark : IconImage);
   }, []);
+
+  if (!mounted) {
+    return (
+      <Image
+        src={IconImage}
+        alt="Joji.Iba"
+        className="w-full h-auto lg:hidden md:inline-block md:w-full"
+        priority
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
+      />
+    );
+  }
 
   return (
     <Image
