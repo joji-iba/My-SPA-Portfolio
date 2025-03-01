4d1455685d28be34e326567a5fd4f1adc5abb2e3
joji <jmry18.0921@gmail.com>
2025-03-01 22:13:59 +0900

    Changed because motion() is deprecated

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,15 +1,13 @@
+'use client';
+
 // アニメーションの実装
 import { motion } from 'framer-motion';
 import Link from 'next/link';
-import React from 'react';
 
-const MotionLink = motion(Link);
-
-function Logo() {
+export default function Logo() {
   return (
     <div className="flex items-center justify-center mt-2">
-      <MotionLink
-        href="/"
+      <motion.div
         className="w-16 h-16 bg-dark text-light flex items-center justify-center rounded-full text-2xl font-bold  border border-solid border-transparent dark:border-light"
         whileHover={{
           backgroundColor: [
@@ -25,10 +23,8 @@ function Logo() {
           },
         }}
       >
-        J.I
-      </MotionLink>
+        <Link href="/">J.I</Link>
+      </motion.div>
     </div>
   );
 }
-
-export default Logo;
