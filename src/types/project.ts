042cac390803e0d4801df4c647e49465fdd348ab
joji <jmry18.0921@gmail.com>
2025-04-06 16:06:10 +0900

    define type

---
+++ src/types/project.ts
@@ -0,0 +1,12 @@
+export interface Project {
+  id: number;
+  title: string;
+  description: string;
+  type: string;
+  image: string;
+  link: string;
+  github: string;
+  featured: boolean;
+  created_at: string;
+  updated_at: string;
+}
