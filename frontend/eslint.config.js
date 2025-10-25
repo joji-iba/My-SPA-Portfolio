5e2192f4f80e32857959be505ff6ec1f37067431
joji <jmry18.0921@gmail.com>
2025-10-25 10:21:43 +0900

    ESLintも最新版にupdate

---
+++ frontend/eslint.config.js
@@ -0,0 +1,54 @@
+import js from '@eslint/js';
+import nextPlugin from 'eslint-config-next';
+import prettierPlugin from 'eslint-plugin-prettier';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import importPlugin from 'eslint-plugin-import';
+
+export default [
+  {
+    ignores: ['node_modules/', '.next/', 'dist/'],
+  },
+  // Merge Next.js configs manually to avoid duplicate plugin definitions
+  ...nextPlugin,
+  // Custom project config (adds TS/React/import/prettier rules on top of Next.js)
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 2020,
+      sourceType: 'module',
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      prettier: prettierPlugin,
+      import: importPlugin,
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react-hooks/set-state-in-effect': 'off',
+      'import/order': 'off',
+      'prettier/prettier': [
+        'error',
+        {
+          trailingComma: 'all',
+          endOfLine: 'lf',
+          semi: true,
+          singleQuote: true,
+          printWidth: 80,
+          tabWidth: 2,
+        },
+      ],
+    },
+    settings: {
+      react: { version: 'detect' },
+      'import/resolver': {
+        typescript: {
+          project: ['./tsconfig.json'],
+        },
+      },
+    },
+  },
+];
