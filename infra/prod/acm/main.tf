2f6a4bbb0125648c6cb3067ab3109f67b7d4965d
joji <jmry18.0921@gmail.com>
2025-12-19 18:42:09 +0900

    get ssl by certificate manager

---
+++ infra/prod/acm/main.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  backend "s3" {
+    bucket  = "tf-state-portfolio-prod"
+    key     = "prod/acm/terraform.tfstate"
+    region  = "ap-northeast-1"
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+data "aws_route53_zone" "primary" {
+  name         = var.hosted_zone_name
+  private_zone = false
+}
+
+resource "aws_acm_certificate" "this" {
+  domain_name       = var.domain_name
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.this.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  zone_id = data.aws_route53_zone.primary.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  ttl     = 60
+  records = [each.value.record]
+}
+
+resource "aws_acm_certificate_validation" "this" {
+  certificate_arn         = aws_acm_certificate.this.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
